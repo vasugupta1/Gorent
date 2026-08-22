116961cbe2c26d5aefe6e7ddb5ad541d7d737995
Vasu Gupta <vasugupta1@gmail.com>
2026-08-22 13:41:52 +0100

    started to create a basic way to get the torrent file and output dir

--- cmd/main.go
+++ cmd/main.go
@@ -1,9 +1,18 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+)
 
 func main() {
 	fmt.Println("Starting Gorent")
+	if len(os.Args) < 3 {
+		fmt.Fprintln(os.Stderr, "Usage: %s <torrent-file> <output-dir>", os.Args[0])
+	}
 
-	fmt.Println("Stopping Gorent")
+	torrentFile := os.Args[1]
+	outputDir := os.Args[2]
+
+	fmt.Printf("Downloading %s to %s...\n", torrentFile, outputDir)
 }
